2db5abb9eca68f08a56968bb3f745ade080dcaeb
EmmanuelSinsel <95643610+EmmanuelSinsel@users.noreply.github.com>
2024-07-01 21:26:35 -0700

    commit inicial

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Dimensions, } from 'react-native';
+import styles from './Styles';
+import Pdf from 'react-native-pdf';
+
 
 export default function App() {
+
+  const source = { uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf', cache: true };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Pdf
+        source={source}
+        onLoadComplete={(numberOfPages,filePath) => {
+            console.log(`Number of pages: ${numberOfPages}`);
+        }}
+        onPageChanged={(page,numberOfPages) => {
+            console.log(`Current page: ${page}`);
+        }}
+        onError={(error) => {
+            console.log(error);
+        }}
+        onPressLink={(uri) => {
+            console.log(`Link pressed: ${uri}`);
+        }}
+        style={styles.pdf}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ Styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    spacer10:{
+        marginBottom:10
+    },
+    spacer20:{
+        marginBottom:20
+    },
+    spacer30:{
+        marginBottom:30
+    },
+    spacer50:{
+        marginBottom:50
+    },
+});
+
+export default styles;
